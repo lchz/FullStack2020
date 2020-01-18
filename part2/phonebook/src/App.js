f6a99b6aa146d36f3fee7ff5014dade4d1194d10
chenhuiz <chenhui.zhao@helsinki.fi>
2020-01-18 23:26:06 +0200

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,15 +3,17 @@ import React, { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '123-456'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([])
 
-  const rows = () => persons.map(p => <p key={p.name}>{p.name} {p.number} </p>)
+  // const rows = () => persons.map(p => <p key={p.name}>{p.name} {p.number} </p>)
 
   // onChange event handler
   const handleNameChange = (event) => {
@@ -43,10 +45,22 @@ const App = () => {
     setNewNumber('')
   }
 
+  // filter onChange event handler
+  const handleFilter = (event) => {
+    setSearch(event.target.value)
+    event.target.value === '' ? setResults([])
+                              : setResults(persons.filter(p => p.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
+  const filterRows = () => results.map(r => <p key={r.name}>{r.name} {r.number} </p>)
+
   return (
     <div>
       <h2>Phonebook</h2>
 
+      filter shown with: <input value={search} onChange={handleFilter} />
+
+      <h3>Add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -60,8 +74,9 @@ const App = () => {
 
       </form>
 
-      <h2>Numbers</h2>
-      {rows()}
+      <h3>Numbers</h3>
+      {/* {rows()} */}
+      {filterRows()}
     </div>
   )
 }
